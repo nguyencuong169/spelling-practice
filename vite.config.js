809635e769821521402ae80ff45e31c1d23159a4
nguyencuong169 <89183788+nguyencuong169@users.noreply.github.com>
2026-03-28 23:50:35 +0700

    Create vite.config.js for Vue 3 configuration

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'modules',
+  },
+});
