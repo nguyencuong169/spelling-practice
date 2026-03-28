29d822caca921146736fae6a99d3928c0a972358
nguyencuong169 <89183788+nguyencuong169@users.noreply.github.com>
2026-03-29 00:11:19 +0700

    Create HomePage.vue for the home page component

---
+++ src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <h1>Welcome to the Spelling Practice App!</h1>
+    <button @click="startQuiz">Start Quiz</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  methods: {
+    startQuiz() {
+      // Logic to start quiz
+    }
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+}
+</style>
